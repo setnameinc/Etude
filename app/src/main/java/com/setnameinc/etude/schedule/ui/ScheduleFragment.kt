c7807d166ce882cef3299ddcb2ad434e2d2462e4
kolovsky <setnameinc@gmail.com>
2019-10-26 15:34:30 +0300

    upgrade coordinator components

--- app/src/main/java/com/setnameinc/etude/schedule/ui/ScheduleFragment.kt
+++ app/src/main/java/com/setnameinc/etude/schedule/ui/ScheduleFragment.kt
@@ -1,10 +1,11 @@
-package com.setnameinc.etude.mainschedule.ui
+package com.setnameinc.etude.schedule.ui
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.setnameinc.etude.R
 import com.setnameinc.etude.bottommenu.ui.BottomMenuFragment
+import com.setnameinc.etude.mainschdule.ui.ScheduleMainFragment
 import com.setnameinc.etude.topmenu.ui.TopMenuFragment
 
 class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
@@ -20,6 +21,9 @@ class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
         )?.commit()
         fragmentManager?.beginTransaction()?.replace(R.id.bottomComponent,
             BottomMenuFragment()
+        )?.commit()
+        fragmentManager?.beginTransaction()?.replace(R.id.mainComponent,
+            ScheduleMainFragment()
         )?.commit()
     }
 
