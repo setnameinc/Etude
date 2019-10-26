03b6f7948b7d9e71ccb28032cba1ea891a486f63
kolovsky <setnameinc@gmail.com>
2019-10-26 19:34:45 +0300

    implement header for main schedule adapter

---
+++ app/src/main/java/com/setnameinc/etude/mainschdule/ui/ScheduleHeaderViewHolder.kt
@@ -0,0 +1,11 @@
+package com.setnameinc.etude.mainschdule.ui
+
+import android.view.View
+
+class ScheduleHeaderViewHolder(
+    val view: View
+) : ScheduleViewHolder<ScheduleItem.ScheduleHeaderItem>(view = view) {
+    override fun bind(item: ScheduleItem.ScheduleHeaderItem) {
+
+    }
+}
